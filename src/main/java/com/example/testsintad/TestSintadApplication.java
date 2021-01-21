d0ca4c0eacab2001f0f73389b9e3fdea76142f48
marceloarmando <mcuellarr98@gmail.com>
2021-01-21 13:50:26 -0500

    initial commit

---
+++ src/main/java/com/example/testsintad/TestSintadApplication.java
@@ -0,0 +1,13 @@
+package com.example.testsintad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSintadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSintadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testsintad/TestSintadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsintad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSintadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
